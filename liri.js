0352585f664cc694b10f66da30b1aafc83b47f8c
developrfw <developrfw@gmail.com>
2019-10-20 16:41:44 -0600

    initial set-up

---
+++ liri.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+var Spotify = require("node-spotify-api");
+var keys = require("./keys.js");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+
+var spotify = new Spotify(keys.spotify);
