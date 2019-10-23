9dba007d719140b1e7dc81e0ed2e57c56a70d513
developrfw <developrfw@gmail.com>
2019-10-22 22:17:52 -0600

    finished product

--- liri.js
+++ liri.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-var request = require("request");
+// var request = require("request");
 var axios = require("axios");
-// var moment = require("moment");
+var moment = require("moment");
 var fs = require("fs");
 
 var artistNames = function (artist) {
@@ -38,77 +38,105 @@ var getSpotify = function (songName) {
 
 }
 
+var getBands = function (artist) {
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  axios.get(queryURL).then(
+    function (response) {
+
+      var jsonData = response.data;
+      if (!jsonData.length) {
+        console.log("No results found for " + artist);
+        return;
+      }
+      console.log("Upcoming concerts for " + artist + ":");
+      for (var i = 0; i < jsonData.length; i++) {
+        var show = jsonData[i];
+        console.log(
+          show.venue.city +
+          "," +
+          (show.venue.region || show.venue.country) +
+          " at " +
+          show.venue.name +
+          " " +
+          moment(show.datetime).format("MM/DD/YYYY")
+
+        );
+      }
+    }
+  );
+};
+
 var getMovie = function (movieName) {
 
-    if (movieName === undefined) {
-      movieName = "Mr Nobody";
+  if (movieName === undefined) {
+    movieName = "Mr Nobody";
+
+  }
+
+
+  var urlRep = 'http://www.omdbapi.com/?t=' + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+  axios.get(urlRep).then(
+    function (response) {
+
+      var jsonData = response.data;
+      console.log("Movie: " + jsonData.Title);
+      console.log("Year: " + jsonData.Year);
+      console.log("Rated: " + jsonData.Rated);
+      console.log("Rating: " + jsonData.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+      console.log("Country: " + jsonData.Country);
+      console.log("Language: " + jsonData.Language);
+      console.log("Plot: " + jsonData.Plot);
+      console.log("Actors: " + jsonData.Actors);
 
     }
 
-
-    var urlRep = 'http://www.omdbapi.com/?t=' + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
-
-    axios.get(urlRep).then(
-      function (response) {
-
-        var jsonData = response.data;
-        console.log("Movie: " + jsonData.Title);
-        console.log("Year: " + jsonData.Year);
-        console.log("Rated: " + jsonData.Rated);
-        console.log("Rating: " + jsonData.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
-        console.log("Country: " + jsonData.Country);
-        console.log("Language: " + jsonData.Language);
-        console.log("Plot: " + jsonData.Plot);
-        console.log("Actors: " + jsonData.Actors);
-
-      }
-
-    );
+  );
 }
 
-var doWhatItSays = function() {
-fs.readFile('random.txt', 'utf8', function (err, data) {
-  if (err) throw err;
+var doWhatItSays = function () {
+  fs.readFile('random.txt', 'utf8', function (err, data) {
+    if (err) throw err;
 
-  var dataArr = data.split(',');
+    var dataArr = data.split(',');
 
-  if (dataArr.length == 2) {
-    pick(dataArr[0], dataArr[1]);
-  } else if (dataArr.length == 1) {
-    pick(dataArr[0]);
-  }
+    if (dataArr.length == 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length == 1) {
+      pick(dataArr[0]);
+    }
 
   });
 
 }
 
-    var pick = function (caseData, functionData) {
+var pick = function (caseData, functionData) {
 
-      switch (caseData) {
+  switch (caseData) {
 
-        case "concert-this":
-          getBands(functionData);
-          break;
-        case "spotify-this-song":
-          getSpotify(functionData);
-          break;
-        case "movie-this":
-          getMovie(functionData);
-          break;
-        case "do-what-it-says":
-          doWhatItSays();
-          break;
-        default:
-          console.log("LIRI doesn't know that");
+    case "concert-this":
+      getBands(functionData);
+      break;
+    case "spotify-this-song":
+      getSpotify(functionData);
+      break;
+    case "movie-this":
+      getMovie(functionData);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("LIRI doesn't know that");
 
-      }
-    };
+  }
+};
 
-    var runThis = function (argOne, argTwo) {
+var runThis = function (argOne, argTwo) {
 
-      pick(argOne, argTwo);
+  pick(argOne, argTwo);
 
-    };
+};
 
-    runThis(process.argv[2], process.argv.slice(3).join(" "));
+runThis(process.argv[2], process.argv.slice(3).join(" "));
